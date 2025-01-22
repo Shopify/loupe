9f99a75708309608e56e05b323a1df0b11fc81e7
Max Bernstein <max.bernstein@shopify.com>
2025-01-22 14:00:10 -0500

    .

--- src/main.rs
+++ src/main.rs
@@ -327,6 +327,7 @@ fn sctp(prog: &mut Program) -> AnalysisResult
             // Handle control instructions first; they do not have a value
             if let Op::IfTrue { val, then_block, else_block } = op {
                 match value_of(val) {
+                    // TODO(max): What if val is Empty? Should we enqueue neither block?
                     Type::Const(Value::Bool(false)) => block_worklist.push_back(*else_block),
                     Type::Const(Value::Bool(true)) => block_worklist.push_back(*then_block),
                     _ => {
