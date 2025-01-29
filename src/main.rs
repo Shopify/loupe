60611657424b43db93108be06682437bdd1a19f2
Maxime Chevalier-Boisvert <maxime.chevalierboisvert@shopify.com>
2025-01-29 17:16:37 -0500

    Use constant

--- src/main.rs
+++ src/main.rs
@@ -1092,25 +1092,27 @@ fn gen_torture_test_2(num_classes: usize, num_roots: usize, dag_size: usize) -> 
             prog.push_ivar(class_id, ivar_name.clone());
 
             // With some probability, initialize the ivar to a random integer
+            // Some ivsrs are left uninitialized
             if rng.pct_prob(80) {
                 let val = Opnd::Const(Value::Int(rng.rand_usize(1, 7) as i64));
                 prog.push_insn(ctor_entry, Op::SetIvar { name: ivar_name, self_val: Opnd::Insn(self_id), val });
             }
         }
 
+        // Constructor returns nil
         prog.push_insn(ctor_entry, Op::Return { val: NIL });
 
         // Create methods for this class
         for j in 0..METHODS_PER_CLASS {
             let (m_id, entry_block) = prog.new_method(class_id, format!("m{}", j));
-            let self_id = prog.push_insn(entry_block, Op::SelfParam);
 
             // TODO: implement increment op with setivar as well?
             // 25% of the time
 
             let ret_val = if rng.rand_bool() {
                 // Choose a random ivar
-                let ivar_idx = rng.rand_index(10);
+                let self_id = prog.push_insn(entry_block, Op::SelfParam);
+                let ivar_idx = rng.rand_index(IVARS_PER_CLASS);
                 let ivar_name = format!("ivar_{}", ivar_idx);
                 let getivar_id = prog.push_insn(entry_block, Op::GetIvar { name: ivar_name, self_val: Opnd::Insn(self_id) });
                 Opnd::Insn(getivar_id)
@@ -1121,10 +1123,7 @@ fn gen_torture_test_2(num_classes: usize, num_roots: usize, dag_size: usize) -> 
                 Opnd::Const(Value::Nil)
             };
 
-            prog.push_insn(
-                entry_block,
-                Op::Return { val: ret_val }
-            );
+            prog.push_insn(entry_block, Op::Return { val: ret_val });
         }
     }
 
